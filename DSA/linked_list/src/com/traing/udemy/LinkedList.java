212d24f5894498f8f9edea7fa098de6e685d10f3
Rohit Bhowmik <rohitbhowmik19@gmail.com>
2024-01-09 01:52:19 +0530

    linked list update 2

--- DSA/linked_list/src/com/traing/udemy/LinkedList.java
+++ DSA/linked_list/src/com/traing/udemy/LinkedList.java
@@ -74,6 +74,29 @@ public class LinkedList {
 		length++;
 	}
 
+	public Node removeFirst() {
+		if (length == 0) return null;
+		Node temp = head;
+		head = head.next;
+		temp.next = null;
+		length--;
+		if (length == 0) {
+			tail = null;
+		}
+		return temp;
+	}
+
+	public Node get(int index) {
+		if (index < 0 || index >= length) {
+			return null;
+		}
+		Node temp = head;
+		for (int i = 0 ; i < index ; i++) {
+			temp = temp.next;
+		}
+		return temp;
+	}
+
 	public void getHead() {
 		System.out.println("Head: " + head.values);
 	}
--- DSA/linked_list/src/com/traing/udemy/Main.java
+++ DSA/linked_list/src/com/traing/udemy/Main.java
@@ -4,15 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        LinkedList myLinkedList = new LinkedList(1);
+        LinkedList myLinkList = new LinkedList(0);
+        myLinkList.append(1);
+        myLinkList.append(2);
+        myLinkList.append(3);
+        myLinkList.append(4);
 
-        myLinkedList.append(2);
-        myLinkedList.append(3);
-        myLinkedList.printList();
+	    System.out.println(myLinkList.get(2).values + "\n");
 
-	    System.out.println("----------");
-
-	    myLinkedList.prepend(34);
-	    myLinkedList.printList();
+        myLinkList.printList();
     }
 }
